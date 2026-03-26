8706898875d9d2d587aeb6910b7abf6ae6e4454d
AurisCode25 <adamelaydi2004@gmail.com>
2026-03-26 18:03:25 +0000

    modified:   csv-test.c     edit: line 17 - repalced "csv_file" with "FileName" in powershell command

--- csv-test.c
+++ csv-test.c
@@ -5,45 +5,48 @@
 #define MAX_LINE_SIZE 1024
 
 int main(void) {
-    char csv_file[MAX_CSV_FILE]; //Since I want this to be able to open any csv file I havent given it a specific name 
+    char csv_file[MAX_CSV_FILE];            //Since I want this to be able to open any csv file I havent given it a specific name 
     char line_size[MAX_LINE_SIZE];
     FILE *explorer_exe;
     FILE *file;
 
-    explorer_exe = popen( //Wanted to use a way to use file explorer to select the csv file, to make the program feel more professional
-        "powershell -Command \"Add-Type -AssemblyName System.Windows.Forms; " 
+    explorer_exe = popen(
+        "powershell -Command \"Add-Type -AssemblyName System.Windows.Forms; "
         "$dialog = New-Object System.Windows.Forms.OpenFileDialog; "
         "$dialog.Filter = 'CSV files (*.csv)|*.csv|All files (*.*)|*.*'; "
-        "if ($dialog.ShowDialog() -eq 'OK') { $dialog.csv_file }\"",
+        "if ($dialog.ShowDialog() -eq 'OK') { $dialog.FileName }\"",
         "r"
     );
     if (!explorer_exe) {
-        perror("Couldn't open powershell.");
+        perror("Couldn't open powershell 1.");
         return 1;
     }
-     if (!fgets(csv_file, sizeof(csv_file), explorer_exe)) {
-        printf("No file selected.\n");
+    
+    if (!fgets(csv_file, sizeof(csv_file), explorer_exe)) {
+        printf("No file selected 2.\n");
         pclose(explorer_exe);
-        return 1;
+        return 2;
     }
-     pclose(explorer_exe);
+    pclose(explorer_exe);
 
     csv_file[strcspn(csv_file, "\n")] = '\0';
 
     if (strlen(csv_file) == 0) {
-        printf("No file selected.\n");
-        return 1;
+        printf("No file selected 3.\n");
+        return 3;
     }
     file = fopen(csv_file, "r");
     if (!file) {
-        perror("Couldn't open File");
-        return 1;
+        perror("Couldn't open File 4.");
+        return 4;
     }
     printf("Contents of %s:\n\n", csv_file);
     while (fgets(line_size, sizeof(line_size), file)) {
         fputs(line_size, stdout);
     }
-    fclose(file);
+    printf("\nPress the ANY key to close.");
+    getchar();
+    fflush(stdin);
     getchar();
     
     return 0;
